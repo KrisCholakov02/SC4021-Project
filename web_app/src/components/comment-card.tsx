36b951759de8ee8153226d7ab425bb2de19681b3
Cholakov Krisityan Kamenov <50497496+KrisCholakov02@users.noreply.github.com>
2024-04-14 12:31:55 +0800

    adding sentiment field

--- web_app/src/components/comment-card.tsx
+++ web_app/src/components/comment-card.tsx
@@ -6,6 +6,18 @@ import parse from 'html-react-parser';
 import MT from '@/utils/MT';
 
 export function CommentCard(result: any) {
+  function getSentimentColor(sentiment: string[]) {
+    let s = sentiment[0];
+    if (s === 'Negative') {
+      return 'bg-red-500';
+    } else if (s === 'Positive') {
+      return 'bg-green-500';
+    } else {
+      return 'bg-yellow-500';
+    }
+  }
+  let color = getSentimentColor(result.Predicted_Class);
+
   return (
     <MT.Card placeholder={undefined} className="!w-full bg-gray-300">
       <MT.CardBody placeholder={undefined}>
@@ -19,11 +31,26 @@ export function CommentCard(result: any) {
           >
             {result.author} • {result.created_utc[0].substring(0, 10)}
           </MT.Typography>
-          <div className="flex w-fit h-fit bg-white p-2 rounded-lg items-center">
-            <MT.Typography variant="paragraph" placeholder={undefined}>
-              Upvotes: <strong>{result.upvotes}</strong>
-            </MT.Typography>
-            <ArrowUpIcon className="ml-2 w-6 h-6" />
+          <div className="flex mb-2">
+            <div
+              className={
+                'flex w-fit h-fit p-2 mr-2 rounded-lg items-center ' + color
+              }
+            >
+              <MT.Typography
+                color={color === 'bg-yellow-500' ? 'black' : 'white'}
+                variant="paragraph"
+                placeholder={undefined}
+              >
+                Sentimet : <strong>{result.Predicted_Class}</strong>
+              </MT.Typography>
+            </div>
+            <div className="flex w-fit h-fit bg-white p-2 rounded-lg items-center">
+              <MT.Typography variant="paragraph" placeholder={undefined}>
+                Upvotes: <strong>{result.upvotes}</strong>
+              </MT.Typography>
+              <ArrowUpIcon className="ml-2 w-6 h-6" />
+            </div>
           </div>
         </div>
         {/* The body of the comment */}
