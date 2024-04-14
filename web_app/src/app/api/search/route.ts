ea3da1424d2cb7baecee052514038725505da6d8
Cholakov Krisityan Kamenov <50497496+KrisCholakov02@users.noreply.github.com>
2024-04-14 16:16:22 +0800

    adding dynamic fuzziness if no results

--- web_app/src/app/api/search/route.ts
+++ web_app/src/app/api/search/route.ts
@@ -82,7 +82,7 @@ export async function POST(req: Request) {
       );
     }
     let responseJSON = await response.json();
-    // If no results are found, try with ~20% fuzziness
+    // If no results are found, try with ~20 fuzziness
     if (responseJSON.response.numFound < 10) {
       console.log('No results found, trying with fuzziness');
       query = `${field}:"${squery}"~20`;
@@ -118,6 +118,50 @@ export async function POST(req: Request) {
         );
       }
       responseJSON = await response.json();
+      // If still no results are found, try with a OR query
+      if (responseJSON.response.numFound < 10) {
+        // Construct the new query (field:terms separated by OR)
+        query = '';
+        let terms = squery.split(' ');
+        for (let i = 0; i < terms.length; i++) {
+          query += `${field}:"${terms[i]}"`;
+          if (i !== terms.length - 1) {
+            query += ' OR ';
+          }
+        }
+        // Add the filters to the query
+        if (filters.subreddit.length > 0) {
+          let subredditQuery = filters.subreddit
+            .map((subreddit: string) => `subreddit:${subreddit}`)
+            .join(' OR ');
+          query += ` AND (${subredditQuery})`;
+        }
+        if (filters.author !== '') {
+          query += ` AND (author:"${filters.author}")`;
+        }
+        if (filters.sentiment !== '') {
+          query += ` AND Predicted_Class:"${filters.sentiment}"`;
+        }
+        if (upvotesQuery !== '') query += ` AND (${upvotesQuery})`;
+        if (dateQuery !== '') query += ` AND (${dateQuery})`;
+
+        encodedQuery = encodeURIComponent(query);
+        solrQueryURL = `http://localhost:8983/solr/comments/select?facet.field=Predicted_Class&facet.field=subreddit&facet=true&q=${encodedQuery}&rows=${numRows}&start=${(page - 1) * numRows}${sortQuery}&spellcheck=true&wt=json`;
+        const response = await fetch(solrQueryURL, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        if (response.status !== 200) {
+          console.error('An error occurred:', response);
+          return Response.json(
+            { error: 'Internal Server Error' },
+            { status: response.status }
+          );
+        }
+        responseJSON = await response.json();
+      }
     }
     console.log(responseJSON);
     let numFound = responseJSON.response.numFound;
